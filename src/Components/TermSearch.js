4c6a9bcf043713af151e4e402758822f936ac35f
pmzarate <patriciamarakzarate@yahoo.com>
2019-12-03 13:29:56 -0600

    additional work on news feed

--- src/Components/TermSearch.js
+++ src/Components/TermSearch.js
@@ -8,7 +8,8 @@ class TermSearch extends Component {
           this.state = {
                 term : '',
                 news : [],
-                author: "",
+                author: '',
+                date: '',
               };
           }
 
@@ -16,9 +17,15 @@ class TermSearch extends Component {
          // ("made it here on click")
           this.setState({ term: e.target.value });
         }
-        authorInput = (e) => {
-          console.log("made it here on click");
+  
+  authorInput = (e) => {
+          //console.log("made it to author");
           this.setState({ author: e.target.value });
+        };
+
+  dateInput = (e) => {
+          //console.log("made it to date")
+          this.setState({ date: e.target.value });
         };
 
   getArticle = (e) => {
@@ -29,33 +36,56 @@ class TermSearch extends Component {
             // console.log (data)
                 if ( data.hits.length === 0 ) {
                   this.setState({ news: [{title: 'No Results found'}]  });
-            } else {
-                 this.setState({ term: '', news: data.hits });
+                } else {
+                  this.setState({ term: '', news: data.hits });
             }})
-          }
-          //   .catch(error => alert(error.message)); 
-          // };
+          .catch(error => alert(error.message)); 
+        };
           
           
   getAuthor = (e) => {
     e.preventDefault();
           fetch(
-              `http://hn.algolia.com/api/v1/search?tags=story,author_${this.state.author}`
+              `http://hn.algolia.com/api/v1/search?tags=author_${this.state.author}`
             )
           .then(res => res.json())
               // .then (res => console.log ("made it"))
           .then(data => {
-                console.log(data.hits, 'author');
-                this.setState({ author: "", news: data.hits });
-              });
+               // console.log(data.hits, 'author');
+                if (data.hits.length === 0) {
+                  this.setState({ news: [{title: 'No Results found'}] });
+                } else {
+                  this.setState({ author: '', news: data.hits });
+                }
+            })
+          .catch(error => alert(error.message));    
           };
-        
+  
+  getByDate = (e) => {
+    e.preventDefault();
+          fetch(
+            `http://hn.algolia.com/api/v1/search_by_date?query`
+                    )
+          .then(res => res.json())
+              // .then (res => console.log ("made it"))
+          .then(data => {
+              // console.log(data.hits, 'date');
+                if (data.hits.length === 0) {
+                  this.setState({ news: [{title: 'No Results found'}] });
+                  } else {
+                  this.setState({ date: '', news: data.hits });
+                  }
+                  })
+          .catch(error => alert(error.message));    
+          };       
+  
 
 render() {
   console.log(this.state)
     return (
       <div className="termSearch">
         {/* <h2>News Feed</h2> */}
+        <br/>
           <form onSubmit ={this.getArticle}>
             <input
               placeholder="search by term/tag"
@@ -64,22 +94,35 @@ render() {
               name="Search term"></input>
                 <button type="submit">submit</button>
           </form>
-          <br/>
-        	<form onSubmit={this.getAuthor}>
-				   	<input
-					  	placeholder="search by Author"
-				  	  onChange={this.authorInput}
-					   	value={this.state.author}
-						  name="Search Author"
-			    		></input>
-				        	<button type="submit">submit</button>
-			    	</form>
-	            <div className="article-list">
+        <br/>
+          <form onSubmit={this.getAuthor}>
+			 	   	<input
+					   	placeholder="search by Author"
+				      onChange={this.authorInput}
+				     	value={this.state.author}
+					    name="Search Author"
+			   	  	></input>
+			        	<button type="submit">submit</button>
+			   	</form>
+        <br/>
+          <form onSubmit={this.getByDate}>
+			 	   	<input
+					   	placeholder="search by Most Recent Articles"
+				      onChange={this.dateInput}
+				     	value={this.state.date}
+					    name="Search By Date"
+			   	  	></input>
+			        	<button type="submit">submit</button>
+			   	</form> 
+	          <div className="article-list">
               {this.state.news.map((a, i) => {
             return (
-              <div key={i} className="article">
-                {a.title} {a.author}
-              </div>
+                <div key={i} className="article">
+                    <h3> {a.title} </h3>
+                    <h5> {a.author}</h5>
+                    <a href={a.url} className='url path'/>
+                  <br/>
+                </div>
             )})}
           </div>
       </div>
